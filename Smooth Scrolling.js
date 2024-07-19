e7eba6b501dd3844681c7d58533f8e0c170fb845
Saqib <prosaqib2022@gmail.com>
2024-07-19 23:55:48 +0500

    smooth scrolling

---
+++ Smooth Scrolling.js
@@ -0,0 +1,6 @@
+$('a[href*="#"]').on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500);
+});
